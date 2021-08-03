ed78417ad8a12eca4960bb432b8bf4670bed1bbe
kwonyongil <ksgkms23@likelion.org>
2021-08-04 00:28:01 +0900

    Initial commit

---
+++ src/main/java/com/example/SSGPaymtCertProject/SsgPaymtCertProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SSGPaymtCertProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgPaymtCertProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsgPaymtCertProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SSGPaymtCertProject/SsgPaymtCertProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SSGPaymtCertProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgPaymtCertProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
