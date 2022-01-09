c17363934efbf4d2322f01545f6b7d519325ee5c
kwonyongil <ksgkms23@likelion.org>
2022-01-09 20:33:35 +0900

    부트스트랩테마 추가, ITEM제거 , ddl update로 변경

--- src/main/java/com/example/SSGPaymtCertProject/exception/ExceptionEnum.java
+++ src/main/java/com/example/SSGPaymtCertProject/exception/ExceptionEnum.java
@@ -13,7 +13,9 @@ public enum ExceptionEnum {
 
     ERROR_USER_BAD_CREDENTIALS(HttpStatus.UNAUTHORIZED, "S0001", "잘못된 회원 정보입니다."),
     ERROR_USER_REDUPLICATION(HttpStatus.UNAUTHORIZED, "S0002", "이미 존재하는 회원입니다."),
-    ERROR_USER_NOTFOUND(HttpStatus.UNAUTHORIZED, "S0002", "존재하지 않는 회원입니다.");
+    ERROR_USER_NOTFOUND(HttpStatus.UNAUTHORIZED, "S0002", "존재하지 않는 회원입니다."),
+
+    ERROR_ITEM_NOTFOUND(HttpStatus.UNAUTHORIZED, "I0002", "존재하지 않는 상품입니다.");
 
     private final HttpStatus status;
     private final String code;
